7a2b7fd1d67375d355b236956b8945ad4020a211
mjhinton <michael.hinton@mail.mcgill.ca>
2015-04-02 17:27:59 -0400

    Major changes made to Critter.

--- TowerDefense/src/critter/Critter.java
+++ TowerDefense/src/critter/Critter.java
@@ -17,196 +17,153 @@ import player.Player;
  */
 abstract public class Critter {
 
-		//private Board gameBoard;
-		private Path gamePath;
-		private Board gameBoard;
-		private Point endPoint;
-		private int currPathIndex;
-		private Point currPathCoord;
-		private Point nextPathCoord;
-		private int pathLength, xInt, yInt, x1, y1; //path length will depend upon the difficulty of the map. (the shorter the easier)
-		private long DEFAULT_DELAY = 1000;//(subject to change)
-		
-		private Image appearance;
-		private double speed; 
-		private int health; 
-		private int reward;
-		private int damage;
-		public Point position; //default init
-		public boolean reachedGoal;
-		
-		public double x;
-		public double y;
-		
-		
-		public Critter(int speed, int health, int reward, int damage, ImageIcon appearance, Board gameBoard){
-			this.gameBoard=gameBoard;
-			this.speed = speed;
-			this.health = health;
-			this.reward = reward;
-			this.damage = damage;
-			this.appearance = appearance.getImage();
-			//this.gameBoard = gameBoard;
-			//System.out.println(gameBoard.toString());
-			this.gamePath = gameBoard.getPath();
-			//System.out.println("umm"+gamePath.print());
-			this.reachedGoal = false;
-			this.xInt = (int) gameBoard.getPath().getCoord(0).getX();
-			System.out.println("somethinge");
-			this.yInt = (int) gameBoard.getPath().getCoord(0).getY();
-			this.position =new Point(xInt,yInt);
-			this.pathLength = gamePath.length();
-			x1 = (int) gameBoard.getPath().getEndCoord().getX();
-			y1 = (int) gameBoard.getPath().getEndCoord().getY();
-			this.endPoint = new Point(x1, y1);
-			//this.position = startPoint; //change back to start point
-		}
-		
-		//(each critter should look different)
-		public void drawCritter(Graphics g){
-			g.drawImage(appearance, (int)(this.x*Map.CELL_PIXEL_SIZE), (int)(this.y*Map.CELL_PIXEL_SIZE), null);
-		}
-		
-		public double getSpeed(){
-			return speed;
-		}
-		
-		public void setSpeed(int i){
-			speed = i; 
-		}
-		
-		public int getHealth(){
-			return health;
-		}
-		
-		public int getWorth(){
-			return reward;
-		}
-		
-		public int getDamage(){
-			return damage;
-		}
-		
-		public Point getPosition(){
-			return position;
-		}
-		
-		public void getsHit(){
-			if (health==0){
-				Player.coins += reward;
-				System.out.println("Critter dead");
-				}
-			else
-				health--;		
-		}
-		
-		
-		public void setHealth(int h){
-			this.health = h;
-		}
-		
-		public void setSpeed(double s){
-			this.speed = s;
-		}
+	// private Board gameBoard;
+	private Path gamePath;
+	private Board gameBoard;
+	private int currPathIndex;
+	private Point currPathCoord;
+	private Point nextPathCoord;
+	private boolean onPath;
+	private int pathLength; // path length will depend upon
+												// the difficulty of the map.
+												// (the shorter the easier)
+	private long DEFAULT_DELAY = 1000;// (subject to change)
 
-		public void increaseDifficulty(double multiplier){ //health and reward increase whenever multiplier is applied
-			this.health*=multiplier;
-			this.reward*=multiplier;
-		}
-		
-		//centercoordinate 
-		public void setDown() throws InterruptedException{
-			int x1;
-			int x2;
-			int y1;
-			int y2;
-			
-			while (health>0)
-			{
-				for (int j= 0; j< pathLength; j++)
-				{	
-					x1 = (int) gamePath.getCoord(j).getX();
-					y1 = (int) gamePath.getCoord(j).getY();
-					//Cannot access gamePath.getCoord(j+1) when at end of path
-					if  (j==pathLength-1){
-						Point p=gameBoard.getMap().getOffMapExit();
-						x2=p.x;
-						y2=p.y;
-					}else{
-						x2 = (int) gamePath.getCoord(j+1).getX();
-						y2 = (int) gamePath.getCoord(j+1).getY();
-					}
-					
-					for (int k = 0; k<((double)abs(x2-x1)/speed)+1; k++){
-						if (x2>x1){
-							position.setLocation(x1+=speed, y1); 
-							if (x2<=x1)
-								position.setLocation(x2, y1);
-						}
-						else if (x1>x2){
-							position.setLocation(x1-=speed,y1);
-							if (x1<=x2)
-								position.setLocation(x2, y1);
-						}
-						else if (y2>y1){
-							position.setLocation(x1,y1+=speed);
-							if (y1<=y2)
-								position.setLocation(x1, y2);
-						}
-						else if (y1>y2){
-							position.setLocation(x1,y1-=speed);
-							if (y2<=y1)
-								position.setLocation(x1, y2);
-						}
-					}
-				}
+	private Image appearance;
+	private double speed;
+	private int health;
+	private int reward;
+	private int damage;
+	public boolean reachedGoal;
 
-					System.out.println(this.toString() + " is moving");
-				
-					if (this.position.getY() >= endPoint.getY() && this.position.getX() >= endPoint.getX())	{
-						reachedGoal = true;
-						System.out.println(this.toString() +" has reached the endpoint");
-						Player.coins -= damage;
-						System.out.println("Player lost "+ damage + " coins");
-						break;
-						}
-				}
+	public double x;
+	public double y;
+
+	public Critter(int speed, int health, int reward, int damage,
+			ImageIcon appearance, Board gameBoard) {
+		this.gameBoard = gameBoard;
+		this.speed = speed;
+		this.health = health;
+		this.reward = reward;
+		this.damage = damage;
+		this.onPath =false;
+		this.appearance = appearance.getImage();
+		this.gamePath = gameBoard.getPath();
+		this.reachedGoal = false;
+		this.pathLength = gamePath.length();
+	}
+
+	// (each critter should look different)
+	public void drawCritter(Graphics g) {
+		if (this.onPath==true && this.health > 0 && this.reachedGoal == false) {
+			g.drawImage(appearance, (int) (this.x * Map.CELL_PIXEL_SIZE),
+				(int) (this.y * Map.CELL_PIXEL_SIZE), null);
+		}
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int i) {
+		speed = i;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public int getWorth() {
+		return reward;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public double getX() {
+		return x;
+	}
+	public double getY(){
+		return y;
+	}
+	public boolean onPath(){
+		return onPath;
+	}
+
+	public void getsHit(int hitDamage) {
+		health=health-hitDamage;
+		if (health <= 0) {
+			health=0;
+			onPath=false;
+			Player.coins += reward;
+			System.out.println("Critter dead");
+		}	
+	}
+
+	public void setHealth(int h) {
+		this.health = h;
+	}
+
+	public void setSpeed(double s) {
+		this.speed = s;
+	}
+
+	public void increaseDifficulty(double multiplier) { // health and reward
+														// increase whenever
+														// multiplier is applied
+		this.health *= multiplier;
+		this.reward *= multiplier;
+	}
+
+	// centercoordinate
+	public void setDown() throws InterruptedException {
+		currPathIndex=0;
+		currPathCoord=gamePath.getCoord(0);
+		x=(double)currPathCoord.x;
+		y=(double)currPathCoord.y;
+		onPath=true;
+	}
+
+	public void updatePosition() {
+		if (this.onPath==true && this.health > 0 && this.reachedGoal == false) {
+			if (currPathIndex == pathLength - 1) {
+				nextPathCoord = gameBoard.getMap().getOffMapExit();
+			} else {
+				nextPathCoord = gamePath.getCoord(currPathIndex + 1);
 			}
-		public void updatePosition(){
-			if (currPathIndex==gamePath.length()-1){
-				nextPathCoord=gameBoard.getMap().getOffMapExit();
-			}else{
-				nextPathCoord=gamePath.getCoord(currPathIndex+1);
-			}
-			double dx=(double)(nextPathCoord.x-currPathCoord.x);
-			double dy=(double)(nextPathCoord.y-currPathCoord.y);
-			
-			//get new board position (double,double)
-			x=x+(dx/Math.abs(dx))*speed;
-			y=y+(dy/Math.abs(dy))*speed;
-			
-			if((int)(Math.floor(x))==nextPathCoord.x && (int)(Math.floor(y))==nextPathCoord.y){
-				if(currPathIndex==gamePath.length()-1){
+			double dx = (double) (nextPathCoord.x - currPathCoord.x);
+			double dy = (double) (nextPathCoord.y - currPathCoord.y);
+
+			// get new board position (double,double)
+			x = x + (dx / Math.abs(dx)) * speed;
+			y = y + (dy / Math.abs(dy)) * speed;
+
+			if ((int) (Math.floor(x)) == nextPathCoord.x
+					&& (int) (Math.floor(y)) == nextPathCoord.y) {
+				if (currPathIndex == pathLength - 1) {
 					reachedGoal = true;
-					System.out.println(this.toString() +" has reached the endpoint");
+					onPath=false;
+					System.out.println(this.toString()
+							+ " has reached the endpoint");
 					Player.coins -= damage;
-					System.out.println("Player lost "+ damage + " coins");
-				}else{
-					currPathCoord=nextPathCoord;
-					currPathIndex=currPathIndex+1;
+					System.out.println("Player lost " + damage + " coins");
+				} else {
+					currPathCoord = nextPathCoord;
+					currPathIndex = currPathIndex + 1;
 				}
-				
+
 			}
-			
 		}
-		
 
-		public abstract boolean getShield();
-		public abstract boolean getDirection();
-		public abstract boolean getVisibility();
-		public abstract String toString();
-		
-		
+	}
 
-	
+	public abstract boolean getShield();
+
+	public abstract boolean getDirection();
+
+	public abstract boolean getVisibility();
+
+	public abstract String toString();
+
 }
