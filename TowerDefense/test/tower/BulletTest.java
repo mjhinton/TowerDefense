4862f6973a731c4b338bc2498b76c8244904ff21
Solvie <lee.solvie@gmail.com>
2015-04-05 03:30:34 -0400

    More test cases added

---
+++ TowerDefense/test/tower/BulletTest.java
@@ -0,0 +1,59 @@
+package tower;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BulletTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMoveBullet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testImplementTargetPattern() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDamageEnemies() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrawBullet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLowestHealth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHighestHealth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClosest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFarthest() {
+		fail("Not yet implemented");
+	}
+
+}
