dca769120ba4292545e9282e8044865b180349b7
Solvie <lee.solvie@gmail.com>
2015-04-05 03:24:55 -0400

    Added JUnit test case skeletons

---
+++ TowerDefense/test/controllers/ControllerTest.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPaintComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGameSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGameSpeed() {
+		fail("Not yet implemented");
+	}
+
+}
