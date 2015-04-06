bae4cf7daf5ab0209f8b003dbd8d9ff7312941ab
Solvie <lee.solvie@gmail.com>
2015-04-05 20:56:05 -0400

    Added test skeletons

---
+++ TowerDefense/test/map/MapTest.java
@@ -0,0 +1,64 @@
+package map;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MapTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMakePathCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakePathStartCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakePathEndCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeSceneryCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToggle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOffMapExit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveMap() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ TowerDefense/test/model/BoardTest.java
@@ -0,0 +1,49 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddTower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveTower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpgradeTower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMap() {
+		fail("Not yet implemented");
+	}
+
+}
