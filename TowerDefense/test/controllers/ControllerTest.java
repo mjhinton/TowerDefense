fc985296674ddf03d51c4cf725e6c5390f5d44d5
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2015-04-06 01:07:13 -0400

    trying to fix git

---
+++ TowerDefense/test/controllers/ControllerTest.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPaintComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testActionPerformed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGameSpeed() {
+		fail("Not yet implemented");
+	}
+
+}
